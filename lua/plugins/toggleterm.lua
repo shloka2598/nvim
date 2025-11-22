ffe562381f396b81c39ffde47afd51c222db6c8a
Shloka Jain <sj@quanta.local>
2025-11-22 19:00:17 +0530

    Update toggleterm config and keymaps

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -8,22 +8,39 @@ return {
 			shell = vim.o.shell,
 		})
 
-		-- Compile and run current C++ file
 		vim.keymap.set("n", "<leader>r", function()
 			vim.cmd("write") -- save file
+
 			local file = vim.fn.expand("%:p")
 			local output = vim.fn.expand("%:p:r")
-			local cmd = string.format("g++ -g -std=c++23 -Wall '%s' -o '%s' && '%s'", file, output, output)
+			local ext = vim.fn.expand("%:e") -- file extension (c / cpp etc.)
+
+			-- choose compiler
+			local compiler = ""
+			local flags = ""
+
+			if ext == "c" then
+				compiler = "gcc"
+				flags = "-g -Wall"
+			elseif ext == "cpp" or ext == "cc" or ext == "cxx" then
+				compiler = "g++"
+				flags = "-g -std=c++23 -Wall"
+			else
+				print("Unsupported file type: " .. ext)
+				return
+			end
+
+			local cmd = string.format("%s %s '%s' -o '%s' && '%s'", compiler, flags, file, output, output)
 
 			local Terminal = require("toggleterm.terminal").Terminal
-			local run_cpp = Terminal:new({
+			local run_file = Terminal:new({
 				cmd = cmd,
 				direction = "float",
 				close_on_exit = false,
 				hidden = true,
 			})
 
-			run_cpp:toggle()
-		end, { desc = "Compile and Run C++" })
+			run_file:toggle()
+		end, { desc = "Compile and Run C/C++" })
 	end,
 }
